de3f041c2e2870e6859d2da5594529d2ed10edd1
Niklas <Max_Niklas@outlook.de>
2024-03-11 09:36:51 +0100

    Structure of the Code refactored

--- src/Organizer/Tabs/SchedulerTab/Scheduler.java
+++ src/Organizer/Tabs/SchedulerTab/Scheduler.java
@@ -5,7 +5,6 @@ import java.awt.*;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.Year;
-import java.util.Arrays;
 import java.util.TreeSet;
 
 public class Scheduler extends JSplitPane {
@@ -13,6 +12,8 @@ public class Scheduler extends JSplitPane {
     private final LocalDate currentDate = LocalDate.now();
     private final AppointmentIO appointmentIO = new AppointmentIO();
     private final AppointmentMap appointmentMap = appointmentIO.getAppointmentMapInstance();
+    private final JComboBox yearChooser = new JComboBox();
+    private final JComboBox monthChooser = new JComboBox();
 
     public Scheduler() {
         setOrientation(JSplitPane.HORIZONTAL_SPLIT);
@@ -21,11 +22,30 @@ public class Scheduler extends JSplitPane {
         setRightComponent(rightPane());
     }
 
+    // in the left Split Pane we have the Appointment - List view component and the appointment control buttons
     private JPanel leftPane() {
         // Panel for the whole left side of the Split Pane (Appointment overview & control Buttons)
         JPanel leftSpacePane = new JPanel(new BorderLayout());
 
+        // List-view of all Appointments:
+        leftSpacePane.add(appointmentListViewPane(), BorderLayout.CENTER);
         // add control buttons for Appointments
+        leftSpacePane.add(appointmentButtonPane(), BorderLayout.SOUTH);
+
+        return leftSpacePane;
+    }
+
+    //Appointment List-view Panel:
+    private JScrollPane appointmentListViewPane() {
+        // setup the Appointment overview (by using a modified JTree embedded in JScrollPane
+        JScrollPane scrollTree = new JScrollPane();
+        //TODO: add JTree into JScrollPane
+
+        return scrollTree;
+    }
+
+    //Appointment control buttons:
+    private JPanel appointmentButtonPane() {
         JPanel appointmentButtonPane = new JPanel(new GridLayout(2, 1));
         JButton newAppointmentButton = new JButton("new appointment");
         JButton delAppointmentButton = new JButton("delete appointment");
@@ -35,22 +55,25 @@ public class Scheduler extends JSplitPane {
         appointmentButtonPane.add(newAppointmentButton);
         appointmentButtonPane.add(delAppointmentButton);
 
-        // setup the Appointment overview (by using a modified JTree embedded in JScrollPane
-        JScrollPane scrollTree = new JScrollPane();
-        //TODO: add JTree into JScrollPane
-
-        // put Buttons and JTree inside one final JPane which gets added to the left side.
-        leftSpacePane.add(scrollTree, BorderLayout.CENTER);
-        leftSpacePane.add(appointmentButtonPane, BorderLayout.SOUTH);
-
-        return leftSpacePane;
+        return appointmentButtonPane;
     }
 
+
+    // Panel for the right Space consisting of Calendar Monthly View & Calendar control Buttons
     private JPanel rightPane() {
-        // Panel for the right Space of the SpiltPane (Calendar Monthly View & Calendar control Buttons)
+
         JPanel rightSpacePane = new JPanel(new BorderLayout());
 
         // Calendar Control elements:
+        rightSpacePane.add(calendarButtonPane(), BorderLayout.NORTH);
+        // Calendar:
+        rightSpacePane.add(calendarPane(), BorderLayout.CENTER);
+
+        return rightSpacePane;
+    }
+
+    // Panel for the control buttons of the calendar
+    private JPanel calendarButtonPane() {
         // Button Control Panel
         JPanel calendarButtonPane = new JPanel();
         BoxLayout boxLayout = new BoxLayout(calendarButtonPane, BoxLayout.X_AXIS);
@@ -65,7 +88,6 @@ public class Scheduler extends JSplitPane {
         // JComboBox to choose easily between years and months
         Year currentYear = Year.of(currentDate.getYear());
         Month currentMonth = Month.of(currentDate.getMonthValue());
-        JComboBox yearChooser = new JComboBox();
         // calendar will always start with 1999
         Year i = Year.of(1999);
         // there will be always +30 years from the year the application gets opened
@@ -73,7 +95,6 @@ public class Scheduler extends JSplitPane {
             yearChooser.addItem(i);
             i = i.plusYears(1);
         }
-        JComboBox monthChooser = new JComboBox();
         for (Month month : Month.values()) monthChooser.addItem(month);
         // set current month and year as selected
         yearChooser.setSelectedItem(currentYear);
@@ -90,17 +111,16 @@ public class Scheduler extends JSplitPane {
         calendarButtonPane.add(nextMonth);
         calendarButtonPane.add(Box.createHorizontalStrut(5));
 
+        return calendarButtonPane;
+    }
 
-        // Calendar:
-        //TODO: add Calendar to rightSpacePane
+    // Panel for the Calendar
+    private JPanel calendarPane() {
         Year yearToCreate = (Year) yearChooser.getSelectedItem();
         Month monthToCreate = (Month) monthChooser.getSelectedItem();
-        TreeSet<Appointment>[] appointmentsOfThisMonth = appointmentMap.getAppointmentsOfMonth(yearToCreate, monthToCreate);
-        MonthPanel monthPanel = new MonthPanel(yearToCreate, monthToCreate, appointmentsOfThisMonth);
+        TreeSet<Appointment>[] appointmentsOfThisMonth = appointmentMap.getAppointmentsOfMonth(yearToCreate,
+                                                                                               monthToCreate);
 
-        rightSpacePane.add(calendarButtonPane, BorderLayout.NORTH);
-        rightSpacePane.add(monthPanel, BorderLayout.CENTER);
-
-        return rightSpacePane;
+        return (new MonthPanel(yearToCreate, monthToCreate, appointmentsOfThisMonth));
     }
 }
