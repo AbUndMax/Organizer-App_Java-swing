65b4b5a456a97b506e20443ea6ce079b5c2e3fdd
Niklas <Max_Niklas@outlook.de>
2024-03-14 18:20:52 +0100

    format on how appointments get saved to txt files changed & write / actualize and delete appointment methods added

--- src/Organizer/Tabs/SchedulerTab/AppointmentIO.java
+++ src/Organizer/Tabs/SchedulerTab/AppointmentIO.java
@@ -1,85 +1,78 @@
 package Organizer.Tabs.SchedulerTab;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.Year;
-import java.util.TreeSet;
 
 public class AppointmentIO {
 
     private final String pathToAppointmentFolder = "Files/SchedulerFiles";
-    private final AppointmentCollection appointmentCollection = new AppointmentCollection();
+    private final AppointmentCollection appointmentCollection;
 
-    public AppointmentIO() {
+    public AppointmentIO(AppointmentCollection appointmentCollection) {
+        this.appointmentCollection = appointmentCollection;
         Year currentYear = Year.of(LocalDate.now().getYear());
-        createNewYearFile(currentYear);
+        createNewYearFolder(currentYear);
         loadYear(currentYear);
-    }
-
-    public AppointmentCollection getAppointmentMapInstance() {
-        return appointmentCollection;
     }
 
     // this Method loads all Appointments of one Year into the System
     public void loadYear(Year year) {
 
-        File file = new File(pathToAppointmentFolder + "/Appointments_" + year.getValue());
+        File directory = new File(pathToAppointmentFolder + "/" + year.getValue());
 
-        try (BufferedReader bReader = new BufferedReader(new FileReader(file))) {
+        File[] files = directory.listFiles();
 
-            String line;
+        for (File file : files) {
+            try (BufferedReader bReader = new BufferedReader(new FileReader(file))) {
 
-            while ((line = bReader.readLine()) != null) {
+                String line;
 
-                if (line.charAt(0) == '#') {
+                while ((line = bReader.readLine()) != null) {
 
                     // new Appointment Instance
                     Appointment appointment = new Appointment();
 
                     //set all Fields:
 
-                    //set Title
+                    //read and set Title
                     line = line.substring(1);
                     appointment.setTitle(line);
-                    //set startDate
+                    //read and set startDate
                     line = bReader.readLine().substring(1);
                     String[] startDate = line.split("-");
                     appointment.setStartDate(getLocalDate(startDate));
-                    //setEndDate
+                    //read and set EndDate
                     line = bReader.readLine().substring(1);
                     String[] endDate = line.split("-");
                     appointment.setEndDate(getLocalDate(endDate));
-                    //set StartTime
+                    //read and set StartTime
                     line = bReader.readLine().substring(1);
                     String[] startTime = line.split(":");
                     appointment.setStartTime(getLocalTime(startTime));
-                    //set EndTime
+                    //read and set EndTime
                     line = bReader.readLine().substring(1);
                     String[] endTime = line.split(":");
                     appointment.setEndTime(getLocalTime(endTime));
-                    //set Repetition
+                    //read and set Repetition
                     line = bReader.readLine().substring(1);
-                    Repetition repetition = Repetition.repetitionOf(line);
+                    Repetition repetition = Repetition.StringToRepetition(line);
                     appointment.setRepetition(repetition);
-                    //set number of repetitions
+                    //read and set number of repetitions
                     line = bReader.readLine().substring(1);
                     int numberOfRepetition = Integer.parseInt(line);
                     appointment.setNumberOfRepetition(numberOfRepetition);
-                    //set Description
+                    //read and set Description
                     line = bReader.readLine().substring(1);
                     appointment.setDescription(line);
 
                     appointmentCollection.addAppointment(appointment.getStartDate(), appointment);
-
                 }
             }
-
-        }
-        catch (Exception expt) {
-            expt.printStackTrace();
+            catch (Exception expt) {
+                expt.printStackTrace();
+            }
         }
     }
 
@@ -97,17 +90,78 @@ public class AppointmentIO {
     }
 
     // creates a new file of the current year, in case no such file exists.
-    private void createNewYearFile(Year year) {
+    private void createNewYearFolder(Year year) {
 
-        File file = new File(pathToAppointmentFolder + "/Appointments_" + year.getValue());
+        File file = new File(pathToAppointmentFolder + "/" + year.getValue());
 
         try {
-            file.createNewFile();
+            file.mkdir();
         }
         catch (Exception expt) {
             expt.printStackTrace();
         }
     }
 
-    //TODO: create methods to write to file & save appointments
+    public void NewAppointmentFile(Appointment appointment) {
+
+        Year appointmentYear = Year.of(appointment.getStartDate().getYear());
+        String pathToDirectory = pathToAppointmentFolder + "/" + appointmentYear;
+        File directory = new File(pathToDirectory);
+
+        if (directory.exists()) {
+            writeAppointmentToFile(appointment);
+        }
+        else {
+            createNewYearFolder(appointmentYear);
+            writeAppointmentToFile(appointment);
+        }
+    }
+
+    public void actualizeAppointmentFile(Appointment oldAppointment, Appointment actualizedAppointment) {
+        File oldFile = new File(getAppointmentPath(oldAppointment));
+        File newFile = new File(getAppointmentPath(actualizedAppointment));
+
+        oldFile.renameTo(newFile);
+
+        writeAppointmentToFile(actualizedAppointment);
+    }
+
+    public void deleteAppointmentFile(Appointment appointment) {
+        File delFile = new File(getAppointmentPath(appointment));
+
+        delFile.delete();
+    }
+
+    private String getAppointmentPath(Appointment appointment) {
+        String part1 = appointment.getStartDate().toString();
+        String part2 = appointment.getStartTime().toString();
+        String part3 = appointment.getEndDate().toString();
+        String part4 = appointment.getEndTime().toString();
+
+        String appointmentFileString =  part1 + "_" + part2 + "_" + part3 + "_" + part4;
+
+        String appointmentYear =  String.valueOf(appointment.getStartDate().getYear());
+
+        return  pathToAppointmentFolder + "/" + appointmentYear + "/" + appointmentFileString;
+    }
+
+    private void writeAppointmentToFile(Appointment appointment) {
+
+        File file = new File(getAppointmentPath(appointment));
+
+        try (PrintWriter pw = new PrintWriter(file)) {
+            pw.println(appointment.getTitle());
+            pw.write(">" + appointment.getStartDate().toString());
+            pw.write(">" + appointment.getEndDate().toString());
+            pw.write(">" + appointment.getStartTime().toString());
+            pw.write(">" + appointment.getEndTime().toString());
+            pw.write(">" + appointment.getRepetition().toString());
+            pw.write(">" + appointment.getNumberOfRepetition());
+            pw.write(">" + appointment.getDescription());
+        }
+        catch (Exception expt) {
+            expt.printStackTrace();
+        }
+    }
+
 }
